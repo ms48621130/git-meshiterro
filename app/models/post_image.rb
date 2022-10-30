ac10415e19136af8d2b885c9699b0c8effabc91b
EC2 Default User <ec2-user@ip-172-31-35-2.ap-northeast-1.compute.internal>
2022-10-30 06:59:54 +0000

    [Add meshiterro]

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -2,6 +2,7 @@ class PostImage < ApplicationRecord
 
  belongs_to :user
  has_many :post_comments, dependent: :destroy
+ has_many :favorites, dependent: :destroy
  has_one_attached :image
 
  def get_image(width, height)
@@ -11,5 +12,10 @@ class PostImage < ApplicationRecord
     end
     image.variant(resize_to_limit: [width, height]).processed
  end
+
+ def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+ end
+
 
 end
